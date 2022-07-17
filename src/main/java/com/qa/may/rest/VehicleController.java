f48957dd1ee20535d19778436dbc00fd92773e83
erhnaks <erhnaks@hotmail.com>
2022-07-17 23:53:28 +0100

    bug-fixed

--- src/main/java/com/qa/may/rest/VehicleController.java
+++ src/main/java/com/qa/may/rest/VehicleController.java
@@ -7,6 +7,7 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.may.entity.Vehicle;
 import com.qa.may.service.VehicleService;
-
+@CrossOrigin
 @RestController
 public class VehicleController {
 
